b0c45f2e25406beb35169a8cb935f8d87b4ab161
jason <yimkuantsang@gmail.com>
2025-04-11 16:53:40 +0800

    01

---
+++ test.js
@@ -0,0 +1,88 @@
+// //alert('hello Javascript');
+// console.log('Javascript')
+// //(Variable)
+// let age =10
+
+// console.log(age,typeof(age));
+// // Integer()
+// // Float()
+// // String
+// // Boolean
+// let a = null;
+// //array
+// let hobbies = ['写程序','玩游戏','hhh',23,true];
+
+// let w=false
+// let person = {
+//     name : ''
+
+// }
+// console.log(a,hobbies,person)
+// let platform='youtube'
+// let lesson='javascript'
+// console.log(platform,'zhiduobao'+lesson) 
+// let isGoodTeather=false;
+// //undefined
+// let b;
+// console.log(b);
+
+// //object
+// let people={
+//     name:'Ho Yin',
+//     ages:30,
+//     goodInProgramming:true,
+//     hobbies:'girl'
+// };
+// console.log(people)
+let lesson='\'dictionary\''
+// console.log(lesson.length)
+// let year=30
+// let userInput='      learning JavaScript    ';
+// console.log(userInput);
+// console.log(userInput.trim());
+// console.log(userInput.trimEnd())
+// console.log(lesson.substr(0,4))
+// console.log(lesson.includes('t'))
+// console.log(lesson.replaceAll('i','o'))
+// let language='javascript';
+// language+='   Hello'
+// console.log(language)
+// let like=10
+// like++
+// console.log(like)
+// let num=12.3445
+// console.log(num.toFixed(2))
+// let pass=true;
+// let mark=60;
+// console.log(mark>=50)
+
+let pass=null;
+// if(mark>=50){
+//     console.log(`Congratulations on your ${mark},pass`)
+// }
+// else{
+//     console.log(`terribly sorry ,your ${mark} fail`)
+// }
+// console.log((mark>=50)?`you have ${mark} fraction,pass`:`you have ${mark} fraction,fail`);}
+// for(let i=1;i<=10;i+=3){
+//     console.log('Log'+i+'times');
+// }
+// let friends=['Ho Yin','kui',"hyne",'Peter','kitty','Heidi'];
+// console.log(friends);
+// console.log(friends[1]);
+
+// for(let f=0;f<friends.length;f++){
+//     console.log('friends #id:'+(f+1)+' '+friends[f])
+// }
+let num=[1,2,3,4,5,6,7,8,9,10];
+let r=0
+while(r<num.length){
+    console.log(num[r]);
+    r++;
+} 
+let i=num.length-1;
+do{
+    console.log(num[i]);
+    i--;
+
+}while(i>=0)
